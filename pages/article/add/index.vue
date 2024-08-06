5bf0bac597fdac34db5986fa83ee1359c8c813ee
liuh-aq <liuh-aq@glodon.com>
2024-08-06 22:57:47 +0800

    feat: 新增文章添加页面

--- pages/article/add/index.vue
+++ pages/article/add/index.vue
@@ -1,15 +1,61 @@
 <script setup lang="ts">
+import Editor from '@/components/wangEditor/index.vue'
+import { reactive, ref, nextTick } from 'vue'
 const formData = reactive({
   title: '',
   content: '',
+  categoryId: '',
   tags: []
 })
+const optionsList = ref([{
+  value: '1',
+  label: '前端'
+}, {
+  value: '2',
+  label: '后端'
+}, {
+  value: '3',
+  label: '数据库'
+}, {
+  value: '4',
+  label: '运维'
+}])
+const formRule = reactive({
+  title: [
+    { required: true, message: '请输入标题', trigger: 'blur' }
+  ],
+  categoryId: [
+    { required: true, message: '请选择分类', trigger: 'change' }
+  ]
+})
+const inputValue: any = ref('')
+const inputVisible = ref(false)
+const InputRef = ref(null)
+
+const handleClose = (tag: string): void => {
+  formData.tags.splice(formData.tags.indexOf(tag), 1)
+}
+
+const showInput = (): void => {
+  inputVisible.value = true
+  void nextTick(() => {
+    InputRef.value?.input?.focus()
+  })
+}
+
+const handleInputConfirm = (): void => {
+  if (inputValue.value) {
+    formData.tags.push(inputValue.value)
+  }
+  inputVisible.value = false
+  inputValue.value = ''
+}
 </script>
 
 <template>
-  <div class="art-add-box">
-    <el-form ref="form" :model="formData" label-width="80px">
-      <el-form-item label="标题">
+  <div class="art-add-box m-t-20">
+    <el-form ref="form" :model="formData" label-width="80px" :rules="formRule">
+      <el-form-item label="标题" prop="title">
         <el-input
           v-model="formData.title"
           placeholder="请输入标题，不超过50个字"
@@ -18,14 +64,43 @@ const formData = reactive({
           clearable
         ></el-input>
       </el-form-item>
+      <el-form-item label="分类" prop="categoryId">
+        <el-select v-model="formData.categoryId" placeholder="请选择文章分类" clearable style="width:100%">
+          <el-option
+            v-for="item in optionsList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="内容">
-        <el-input
-          v-model="formData.title"
-          placeholder="请输入标题，不超过50个字"
-          max="50"
-          show-word-limit
-          type="textarea"
-        ></el-input>
+          <Editor></Editor>
+      </el-form-item>
+      <el-form-item label="标签">
+        <div class="tag-box">
+          <el-tag
+            class="m-r-6"
+            v-for="tag in formData.tags"
+            :key="tag"
+            closable
+            :disable-transitions="false"
+            @close="handleClose(tag)"
+          >
+            {{ tag }}
+          </el-tag>
+          <el-input
+            v-if="inputVisible"
+            ref="InputRef"
+            v-model="inputValue"
+            size="small"
+            @keyup.enter="handleInputConfirm"
+            @blur="handleInputConfirm"
+          />
+          <el-button v-else class="button-new-tag" size="small" @click="showInput">
+            + 添加标签
+          </el-button>
+        </div>
       </el-form-item>
     </el-form>
   </div>
@@ -37,5 +112,8 @@ const formData = reactive({
     width: 100%;
     height: 100%;
   }
+}
+.tag-box{
+  display: flex;
 }
 </style>
